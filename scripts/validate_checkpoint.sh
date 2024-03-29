31ccd0e3bab3d86c2d471ab5de0018febee07ebd
Artur Shatveryan <artur.shatveryan@picsart.com>
2024-03-29 07:56:58 -0500

    changed file name

--- scripts/validate_checkpoint.sh
+++ scripts/validate_checkpoint.sh
@@ -1,4 +1,4 @@
 # For validation purposes
 
 
-python ../src/validation.py test_file.parquet --checkout_path classifier_checkpoint.pth --device cuda --batch_size 1024 --num_workers 9
+python ../src/validate.py test_file.parquet --checkout_path classifier_checkpoint.pth --device cuda --batch_size 1024 --num_workers 9
